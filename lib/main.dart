ada0693fa55621be434caa352136e3855cededdf
Rifai-Hub <rifaigusnian1401@gmail.com>
2025-11-20 09:50:47 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:attendance_app/ui/home_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        // Add your own Firebase project configuration from google-services.json
+        apiKey: 'AIzaSyBvPRr9MsM-vzvXe8OhfsZB3G3EBsU7Kiw', 
+        appId:
+            '1:448183400370:android:a7b2e4508523139e9f96ab', 
+        messagingSenderId: '448183400370',
+        projectId: 'attendance-app-a1762', 
+      ),
+    );
+    // Firebase connection success
+    print("Firebase Terhubung ke:");
+    print("API Key: ${Firebase.app().options.apiKey}");
+    print("Project ID: ${Firebase.app().options.projectId}");
+  } catch (e) {
+    // Firebase connection failed
+    print("Firebase gagal terhubung: $e");
+  }
+  // runApp(const HomeScreen());
+  runApp(const TestApp());
+}
+
+class TestApp extends StatelessWidget {
+  // Main App
+  const TestApp({super.key}); // Constructor of TestApp clas
+
+  @override // can give information about about your missing override code
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // remove debug banner
+      home: const HomeScreen(), // HomeScreen class
+    );
+  }
+}
